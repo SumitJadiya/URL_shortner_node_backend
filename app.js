e2e2c7d18f5367cec89015882244bdf4d267d46e
Sumit Jadiya <sumit.jadiya@appdirect.com>
2021-07-07 20:45:42 +0530

    connect to db

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 
+const mysql = require("mysql")
 const express = require("express")
 const cors = require('cors')
 const app = express()
 app.use(cors())
 
 // port
-const port = process.env.DATABASE_PORT;
+const port = process.env.SERVER_PORT;
+
+// connection to db
+var connection = mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: process.env.DBDATABASE
+});
+
+connection.connect();
 
 // APIs
 app.get('/', (req, res) => {
@@ -15,6 +26,8 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
     res.send('test')
 })
+
+connection.end();
 
 // server
 app.listen(port, () => console.log(`Server Running at ${port}`))
