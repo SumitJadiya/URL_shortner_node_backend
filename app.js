2981de2ea43a88a5c8fa853557ccf36de5b33c3f
Sumit Jadiya <sumit.jadiya@appdirect.com>
2021-07-07 20:27:10 +0530

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 require("dotenv").config();
 
 const express = require("express")
-
+const cors = require('cors')
 const app = express()
-app.use(express.json({ extended: false }))
+app.use(cors())
 
-
+// port
 const port = process.env.DATABASE_PORT;
 
+// APIs
+app.get('/', (req, res) => {
+    res.send('home')
+})
+app.get('/test', (req, res) => {
+    res.send('test')
+})
+
+// server
 app.listen(port, () => console.log(`Server Running at ${port}`))
